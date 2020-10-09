6af8d421d29c4ded4e3c0f32d7e467921508ac7b
Ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-10-09 08:45:46 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,11 @@ BOARD_VENDOR := xiaomi
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-04-01
-
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/xiaomi/merlin
+TARGET_KERNEL_CONFIG := atoll-perf_defconfig
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.1-service.xml
